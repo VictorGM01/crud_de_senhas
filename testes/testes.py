4d67ba3a7dcc7bacbfc9742fce31cc54e40bcf78
Victor G. Marques <victormarques8801@gmail.com>
2021-11-20 20:59:56 -0300

    Testa função que exclui as senhas

--- testes/testes.py
+++ testes/testes.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from CRUD.main import insere_valores
+from CRUD.main import insere_valores, exclui_valores
 
 
 class TesteCrud(TestCase):
@@ -12,3 +12,6 @@ class TesteCrud(TestCase):
 
     def test_deve_inserir_uma_nova_senha_quando_a_funcao_insere_valores_for_chamada(self):
         insere_valores(self.programa, self.senha)
+
+    def test_deve_excluir_uma_senha_quando_a_funcao_exclui_valores_for_chamada(self):
+        exclui_valores(self.programa)
