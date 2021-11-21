73e4a95f9babc8500ce1c6aebf09d6cee620e071
Victor G. Marques <victormarques8801@gmail.com>
2021-11-20 22:11:49 -0300

    testa função de update, atualizando senha do programa github

--- testes/testes.py
+++ testes/testes.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from CRUD.main import insere_valores, exclui_valores
+from CRUD.main import insere_valores, exclui_valores, atualiza_valores
 from CRUD.excecoes import DelecaoInvalida
 
 
@@ -13,11 +13,15 @@ class TesteCrud(TestCase):
 
     def test_deve_inserir_uma_nova_senha_quando_a_funcao_insere_valores_for_chamada(self):
         insere_valores(self.programa, self.senha)
+        insere_valores('Facebook', '123456')
 
     def test_deve_excluir_uma_senha_quando_a_funcao_exclui_valores_for_chamada(self):
         exclui_valores(self.programa)
 
-    def test_nao_deve_permitir_delecao(self):
+    def test_nao_deve_permitir_delecao_quando_o_programa_nao_for_encontrado(self):
         with self.assertRaises(DelecaoInvalida):
             exclui_valores('SATURNO')
             exclui_valores(3)
+
+    def test_deve_permitir_atualizacao_de_senha_quando_a_funcao_atualiza_valores_for_chamada(self):
+        atualiza_valores(self.programa, '141598')
